c80f405ba857cd160e4a52e858b6f757d22414a2
MohamedAliBouaziz <medalibouaziz13@gmail.com>
2024-12-04 19:15:19 +0100

    push

--- src/Components/categories/Listcategories.jsx
+++ src/Components/categories/Listcategories.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+const Listcategories = () => {
+    const [categorie, setCategories] = useState([]);
+    const fetchCategories = async () => {
+        try {
+            const res = await axios.get("http://127.0.0.1:8000/api/categories")
+            setCategories(res.data)
+            console.log("tesrt",res)
 
-function Listcategories() {
-  return (
-    <div>
-      liste des categories
-    </div>
-  )
+        } catch (error) {
+            console.log("error")
+        }
+    }
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
+    return (
+        <div>
+            liste des categories
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Image categorie</th>
+                        <th>nom categorie</th>
+                        <th>update</th>
+                        <th>delete</th>
+                    </tr>
+                </thead>
+                {/* <tbody>
+                    {
+                        categorie.map((cat,index)=>
+
+                        )             }
+                </tbody> */}
+            </table>
+                
+        </div>
+    )
+
 }
-
 export default Listcategories
